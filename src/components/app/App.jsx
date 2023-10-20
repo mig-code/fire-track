467ab3bbf74feb8f60dec6bbedfb70be39f6f9fe
Miguel <miguel.aempezar@gmail.com>
2023-10-20 11:37:31 +0200

    🚧 add table with mock data

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 import FiresFilter from '../fires.filter/fires.filter';
 import FiresMap from '../fires.map/fires.map';
 import FiresTable from '../fires.table/fires.table';
@@ -5,14 +7,21 @@ import Footer from '../footer/footer';
 import Header from '../header/header';
 import Pagination from '../pagination/pagination';
 import './App.scss';
+import { listOfFires } from '../../mocks/fires';
 
 const App = () => {
+  const [fires, setFires] = useState([]);
+
+  useEffect(() => {
+    setFires(listOfFires.results);
+  }, []);
+
   return (
     <div className='layout-container'>
       <Header />
       <div className='page-container'>
         <FiresFilter></FiresFilter>
-        <FiresTable></FiresTable>
+        <FiresTable fires={fires}></FiresTable>
         <Pagination></Pagination>
         <FiresMap></FiresMap>
       </div>
